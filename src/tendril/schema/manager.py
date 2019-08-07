02601dc3ce308979a93d35e0a7b4a624b627262d
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-08-07 20:32:25 +0530

    Added doc headers

--- src/tendril/schema/manager.py
+++ src/tendril/schema/manager.py
@@ -17,6 +17,10 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Tendril Schema Manager (:mod:`tendril.schema.manager`)
+======================================================
+"""
 
 
 import importlib
--- src/tendril/validation/schema.py
+++ src/tendril/validation/schema.py
@@ -17,6 +17,10 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Validation elements for Schema Declarations
+===========================================
+"""
 
 
 from tendril.validation.base import ValidationPolicy
